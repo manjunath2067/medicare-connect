5fbe432bafbee256de97e059982a26e2a17d351e
Manjunath Tirakannavar <manjunathbt2067@gmail.com>
2025-04-29 14:44:00 +0530

    Initial Commit

---
+++ src/main/java/com/example/medicare/connect/MedicareConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicare.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicareConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medicare/connect/MedicareConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicare.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
